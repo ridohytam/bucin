17dbdca76e36f0fd3e0a3194582dcafb97a0c34f
ambarido <prankoficial2@gmail.com>
2025-03-27 00:44:30 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Aku Punys Sesuatu Untuk Kamu Nisa Sayang'.split('')
+const text = 'Ini Buat Nisa Pacar Aku'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
